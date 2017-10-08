90818718f979db1ddf7ee389116551bfd5e0c557
Benjamin Ta <benjaminta@berkeley.edu>
2017-10-07 18:16:47 -0700

    more graphics

--- proj/AndroidPickup/app/src/main/java/com/example/benjaminta/androidpickup/MainActivity.java
+++ proj/AndroidPickup/app/src/main/java/com/example/benjaminta/androidpickup/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.view.Window;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,11 +12,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
     }
 
     public void register(View v) {
         Intent intent = new Intent (this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void profile(View v) {
+        Intent intent = new Intent (this, ProfileActivity.class);
         startActivity(intent);
     }
 }
--- proj/AndroidPickup/app/src/main/java/com/example/benjaminta/androidpickup/ProfileActivity.java
+++ proj/AndroidPickup/app/src/main/java/com/example/benjaminta/androidpickup/ProfileActivity.java
@@ -2,13 +2,12 @@ package com.example.benjaminta.androidpickup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
 
-public class RegisterActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register2);
+        setContentView(R.layout.activity_profile);
     }
 }
